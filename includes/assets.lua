317f801e4f4438674d1e63b882308169b7ae27d5
Gahxzafe <zafegahx@gmail.com>
2025-10-02 22:14:37 -0300

    Add files via upload

---
+++ includes/assets.lua
@@ -0,0 +1,14 @@
+SMODS.Atlas({
+    key = "modicon", 
+    path = "ModIcon.png", 
+    px = 34,
+    py = 34,
+    atlas_table = "ASSET_ATLAS"
+})
+
+SMODS.Atlas{
+    key = 'the_deck',
+    path = 'deck.png',
+    px = 71,
+    py = 95
+}
---
+++ includes/deck.lua
@@ -0,0 +1,16 @@
+SMODS.Back{
+    name = "The Deck",
+    key = "i_wanna_be_the_deck",
+    atlas = "the_deck",
+    pos = {x = 0, y = 0},
+    config = {
+        ante_scaling = 0.5, 
+    },
+    loc_txt = {
+        name = "The Deck",
+        text = {
+            "Blinds scale",
+            "is halved"
+        },
+    },
+}
